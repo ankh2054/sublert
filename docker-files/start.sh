56e92973e02b08c5b779b3a3a8f6a0ed50d9bc01
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2020-07-23 13:26:54 +0100

    change to bash

--- docker-files/start.sh
+++ docker-files/start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+
 add_urls() {
     # Out DOMAINS ENV as plaintext
    declare -a DOMAINS="$DOMAINS"
    for domain in "${DOMAINS[@]}"
     do
     : 
-    /usr/bin/python3/python3 /root/sublerts/sublert.py -u $domain -q true
+    /usr/bin/python3/python3 sublert.py -u $domain -q true
     done
 
 
@@ -32,14 +33,15 @@ EOF
 }
 
 slack_nitification(){
-/usr/bin/python3 /root/sublerts/sublert.py -r -l
+/usr/bin/python3 sublert.py -r -l
 }
 
 
 run_subalert(){
+
 while [ 1 ]
 do
-	/usr/bin/python3 /root/sublerts/sublert.py -r -l >> /root/sublerts/sublert.log 2>&1
+	/usr/bin/python3 sublert.py -r -l >> sublert.log 2>&1
 	sleep 43200 # Run every 12 hours 
 done
 }
